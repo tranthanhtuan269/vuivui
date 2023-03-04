98f70556c2e3fb244862c3b3f550bf252ad330a5
tranthanhtuan269 <tran.thanh.tuan269@gmail.com>
2023-03-04 10:23:05 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function login(Request $request){
+        return response()->json([
+            'code' => 200,
+            'username' => $request->username,
+            'password' => $request->password
+        ]);
+    }
+}
