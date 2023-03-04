a2bc0d647ec87f21fb89991107e4bf695730e027
tranthanhtuan269 <tran.thanh.tuan269@gmail.com>
2023-03-04 10:44:26 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function login(Request $request){
+        dd(1);
         return response()->json([
             'code' => 200,
             'username' => $request->username,
